3788c1b0e8d6434c21535c007e98c5964537f3bc
zsq <zhangshuqing912@163.com>
2017-05-25 17:58:54 +0800

    1

--- BonjourSocketClient/BonjourSocketClient.m
+++ BonjourSocketClient/BonjourSocketClient.m
@@ -7,11 +7,11 @@
 //
 
 #import "BonjourSocketClient.h"
-#import "GCDAsyncSocket.h"
-@interface BonjourSocketClient () <GCDAsyncSocketDelegate, NSNetServiceBrowserDelegate, NSNetServiceDelegate>
+#import "AsyncSocket.h"
+@interface BonjourSocketClient () <NSNetServiceBrowserDelegate, NSNetServiceDelegate>
 
 @property (nonatomic, copy) NSString *serverName;
-@property (nonatomic, strong) GCDAsyncSocket *clientSocket;
+@property (nonatomic, strong) AsyncSocket *clientSocket;
 @property (nonatomic, strong) NSNetServiceBrowser *serverBroeser;
 @property (nonatomic, strong) dispatch_queue_t socketQueue;
 @property (nonatomic, strong) NSMutableArray *serverAddresses;
@@ -45,32 +45,69 @@
     [self.clientSocket disconnect];
     self.clientSocket = nil;
     
-    self.netService.delegate = nil;
-    self.netService = nil;
-    
     [self.serverBroeser stop];
     self.serverBroeser = nil;
-    
 }
 
 - (void)connect:(NSNetService *)service{
-    
-    
-    self.clientSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:self.socketQueue];
-    self.clientSocket.userData = @"12345678";
+
+    self.clientSocket = [[AsyncSocket alloc] initWithDelegate:self];
     [self.clientSocket readDataWithTimeout:-1 tag:100];
-    NSError *error = nil;
-    [self.clientSocket connectToHost:service.hostName onPort:service.port error:&error];
-    if (error) {
-        NSLog(@"client连接失败: %@", error);
-        self.clientSocket.delegate = nil;
-        self.clientSocket = nil;
-        [self performSelector:@selector(findServer:) withObject:self.serverName afterDelay:2];
-    } else {
-        NSLog(@"client连接成功");
+    
+    BOOL done = NO;
+    
+    while (!done && ([self.serverAddresses count] > 0))
+    {
+        NSData *addr;
+        
+        // Note: The serverAddresses array probably contains both IPv4 and IPv6 addresses.
+        //
+        // If your server is also using GCDAsyncSocket then you don't have to worry about it,
+        // as the socket automatically handles both protocols for you transparently.
+        
+        if (YES) // Iterate forwards
+        {
+            addr = [self.serverAddresses objectAtIndex:0];
+            [self.serverAddresses removeObjectAtIndex:0];
+        }
+        else // Iterate backwards
+        {
+            addr = [self.serverAddresses lastObject];
+            [self.serverAddresses removeLastObject];
+        }
+        
+        NSLog(@"Attempting connection to %@", addr);
+        
+        NSError *err = nil;
+        if ([self.clientSocket connectToAddress:addr error:&err])
+        {
+            done = YES;
+        }
+        else
+        {
+            NSLog(@"Unable to connect: %@", err);
+        }
+        
     }
     
-    [[NSRunLoop currentRunLoop] run];
+    if (!done)
+    {
+        NSLog(@"Unable to connect to any resolved address");
+    }
+
+    
+//    NSError *error = nil;
+//    [self.clientSocket connectToHost:service.hostName onPort:service.port error:&error];
+//    if (error) {
+//        NSLog(@"client连接失败: %@", error);
+//        self.clientSocket.delegate = nil;
+//        self.clientSocket = nil;
+//        [self performSelector:@selector(findServer:) withObject:self.serverName afterDelay:2];
+//    } else {
+//        NSLog(@"client连接成功");
+//    }
+//    
+//    [[NSRunLoop currentRunLoop] run];
 
 }
 - (void)sendMessage:(NSString *)message{
@@ -123,35 +160,29 @@
 
 
 #pragma mark - GCDAsyncSocket 代理
-
-
-- (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port{
+- (void)onSocket:(AsyncSocket *)sock didConnectToHost:(NSString *)host port:(UInt16)port{
     NSLog(@"socket连接到host: %@, %d", host, port);
-}
-
-
-- (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag{
-    
-    NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-     NSLog(@"BonjourSocket收到消息: %@", str);
     NSData *data1 = [@"123" dataUsingEncoding:NSUTF8StringEncoding];
+    
     [sock writeData:data1 withTimeout:-1 tag:100];
 }
 
+- (void)onSocket:(AsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag{
+    NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"BonjourSocket收到消息: %@", str);
+    NSData *data1 = [@"500" dataUsingEncoding:NSUTF8StringEncoding];
+    [sock writeData:data1 withTimeout:-1 tag:500];
 
-- (void)socket:(GCDAsyncSocket *)sock didWriteDataWithTag:(long)tag{
+}
+- (void)onSocket:(AsyncSocket *)sock didWriteDataWithTag:(long)tag{
     [sock readDataWithTimeout:-1 tag:tag];
 }
 
-- (void)socketDidCloseReadStream:(GCDAsyncSocket *)sock{
-    NSLog(@"读字节流关闭");
-    [self findServer:self.serverName];
-}
-
-- (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(nullable NSError *)err{
-    NSLog(@"socket断开连接: %@", sock.userData);
+- (void)onSocketDidDisconnect:(AsyncSocket *)sock{
+    NSLog(@"socket断开连接");
     
     [self findServer:self.serverName];
+
 }
 
 
